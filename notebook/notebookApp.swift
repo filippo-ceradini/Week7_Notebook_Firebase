fd8e2853be90c84f553262be4a8d94d23c503190
filippo-ceradini <90599585+filippo-ceradini@users.noreply.github.com>
2023-02-08 15:18:05 +0100

    Initial Commit

---
+++ notebook/notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  notebookApp.swift
+//  notebook
+//
+//  Created by Filippo on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
